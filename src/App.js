b3131920c39fb1b48614796ddbc206c4938db4bb
gabminamedez <gabminamedez@gmail.com>
2021-02-17 17:03:17 +0800

    Changed to COVID-19 cases dataset; Need to fix bar chart and add customizations

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
 import { useState, useEffect } from 'react';
 import { Container } from 'reactstrap';
-import ReactApexChart from 'react-apexcharts';
 import Papa from 'papaparse';
+import ReactApexChart from 'react-apexcharts';
 
-import dataset from './nba.csv';
+import Chart from './components/Chart';
+import dataset from './covid.csv';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const data = [];
-
 function App() {
-  const [results, setResults] = useState([]);
+  const [dates, setDates] = useState([]);
+  const [cases, setCases] = useState([]);
+  const [deaths, setDeaths] = useState([]);
+  const [recoveries, setRecoveries] = useState([]);
 
   useEffect(() => {
     Papa.parse(dataset, {
       download: true,
       header: true,
       complete: function(res) {
-          console.log("Finished:", res.data[0]);
-          setResults(res.data);
+        for(var i = 0; i < res.data.length; i++){
+          setDates(dates => [...dates, String(res.data[i].Date)]);
+          setCases(cases => [...cases, parseInt(res.data[i].New)]);
+          setDeaths(deaths => [...deaths, parseInt(res.data[i].Deaths)]);
+          setRecoveries(recoveries => [...recoveries, parseInt(res.data[i].Recoveries)]);
+        }
       }
     });
   }, []);
 
+  const series = [
+    {
+      name: "Cases",
+      data: cases
+    },
+    {
+      name: "Deaths",
+      data: deaths
+    },
+    {
+      name: "Recoveries",
+      data: recoveries
+    }
+  ];
+
+  const options = {
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      categories: dates
+    },
+    tooltip: {
+      x: {
+        format: "MM/dd/yy"
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Container>
-        <h1>Yeet</h1>
+        <header><h1><b>COVID-19 Cases Dashboard</b></h1></header>
+        <hr></hr>
 
-        {Object.entries(results).map(([key, value]) => {
-          return (
-            <p key={key}>{value.player}</p>
-          );
-        })}
+        <ReactApexChart
+          series={series}
+          options={options}
+          type="area"
+          height={350}
+        />
       </Container>
     </div>
   );
